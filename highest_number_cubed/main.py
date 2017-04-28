f52dc29983ed10c1c656cf46d33226ee3997599a
Jacquan McClinton <nauqcaj@gmail.com>
2017-04-28 01:23:14 +0000

    Initial Commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -1,5 +1,36 @@
 """This is the entry point of the program."""
 
-
 def highest_number_cubed(limit):
-    pass
+    
+    previous_number = 1 
+    
+    while True:
+        current_number = previous_number + 1
+        if current_number ** 3 > limit:
+            return previous_number
+
+        previous_number = current_number
+
+#Altermative Answer
+'''
+    number = 0
+    
+    while True:
+        number += 1
+        if number ** 3 > limit:
+            return number -1
+'''
+
+
+
+'''
+def highest_number_cubed(limit):
+    previous_number = 1
+    
+    while True:
+        current_number = previous_number + 1
+        if current_number ** 3 > limit:
+            return previous_number
+            
+        previous_number = current_number
+'''
